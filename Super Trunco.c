565ef3eb072b1890c614b19a89583e884a36bab0
vinnicsilva <vsilvalopes077@gmail.com>
2025-06-06 15:58:01 -0300

    Update

--- Super Trunco.c
+++ Super Trunco.c
@@ -1,127 +1,111 @@
 #include <stdio.h>
 
-void calcular_dados() {
-    // Declaração de Variáveis
-    int populacao;                       //População da Cidade
-    float area;                          //Área em KmÂ²
-    float pib;                           //PIB (Produto Interno Bruto)
-    float densidade_demografica;        //Densidade Demográfica
-    float pib_per_capita;                //PIB per Capita
-
-    // Cálculo da Densidade Demográfica
-    densidade_demografica = populacao / area;
-
-    // Cálculo do PIB per Capita
-    pib_per_capita = pib / populacao;
+// Função que para calcular densidade e pib per capita
+void calcular_dados(int populacao, float area, float pib, float *densidade, float *pib_per_capita) {
+    *densidade = populacao / area;
+    *pib_per_capita = pib / populacao;
 }
 
 int main() {
+    // Declaração de Variáveis
+    char estado[50];                    // Nome do Estado
+    char codigo_da_carta[50];           // Código da Carta
+    char nome_da_cidade[50];            // Nome da Cidade
+    int populacao;                      // População da Cidade
+    float area;                         // Área em km²
+    float pib;                          // PIB (Produto Interno Bruto)
+    int pontos_turisticos;              // Número de Pontos Turísticos
+    float densidade_demografica;        // Densidade Demográfica
+    float pib_per_capita;               // PIB per Capita
 
-   // Declaração de Variáveis
-   char estado [50];                    //Nome do Estado
-   char codigo_da_carta[50];            //Código da Carta
-   char nome_da_cidade[50];             //Nome da Cidade
-   int populacao;                       //População da Cidade
-   float area;                          //Área em KmÂ²
-   float pib;                           //PIB (Produto Interno Bruto)
-   int pontos_turisticos;               //Número de Pontos Turisticos
-   float densidade_demografica;           //Densidade Demográfica
-   float pib_per_capita;                  //PIB per Capita
-
- 
-   //Cadastro da Carta (01)
+    // Cadastro da Carta 1
     printf("\n=== Cadastro de Carta ===\n");
-
     printf("\n=== Cadastre a primeira Carta ===\n");
-   // Solicita e Armazena os Dados da Carta (01)
 
     printf("Digite o Nome do Estado:\n");
-    fgets(estado, 50, stdin); // Lê o Nome do Estado
+    fgets(estado, 50, stdin);
 
-    printf("Digite o Código da Carta: \n");
-    scanf("%s", &codigo_da_carta); // Lê o Código da Carta
-    getchar();  // Remove o caractere de nova linha deixado pelo scanf
+    printf("Digite o Código da Carta:\n");
+    scanf("%s", codigo_da_carta);
+    getchar();
 
-    printf("Digite o Nome da Cidade: \n");
-    fgets(nome_da_cidade, 50, stdin); // Lê o Nome da Cidade
-      
-    printf("Digite a População: \n");
-    scanf("%d", &populacao); // Lê a População
-    getchar();  // Remove o caractere de nova linha deixado pelo scanf
+    printf("Digite o Nome da Cidade:\n");
+    fgets(nome_da_cidade, 50, stdin);
 
-    printf("Digite a Área: \n");
-    scanf("%f", &area); // Lê a Área
-    getchar();  // Remove o caractere de nova linha deixado pelo scanf
+    printf("Digite a População:\n");
+    scanf("%d", &populacao);
+    getchar();
 
-    printf("Digite o PIB: \n");
-    scanf("%f", &pib); // Lê o PIB
-    getchar();  // Remove o caractere de nova linha deixado pelo scanf
+    printf("Digite a Área:\n");
+    scanf("%f", &area);
+    getchar();
 
-    printf("Digite a Quantidade de Pontos Turisticos: \n");
-    scanf("%d", &pontos_turisticos); // Lê a quantidade de pontos turísticos
-    getchar();  // Remove o caractere de nova linha deixado pelo scanf
-   
-    // Calculo para o PIB e Densidade Populacional
-    calcular_dados();
+    printf("Digite o PIB:\n");
+    scanf("%f", &pib);
+    getchar();
 
-    //Exibe os Dados da Carta (01)
+    printf("Digite a Quantidade de Pontos Turísticos:\n");
+    scanf("%d", &pontos_turisticos);
+    getchar();
 
+    // Cálculo dos dados
+    calcular_dados(populacao, area, pib, &densidade_demografica, &pib_per_capita);
+
+    // Exibe os Dados da Carta 1
     printf("\n=== Dados da Primeira Carta ===\n");
     printf("Estado: %s", estado);
     printf("Código da Carta: %s\n", codigo_da_carta);
     printf("Cidade: %s", nome_da_cidade);
     printf("População: %d habitantes\n", populacao);
     printf("Área: %.2f km²\n", area);
-    printf("PIB: %.2f bilhões\n", pib);
-    printf("Pontos turísticos: %d \n", pontos_turisticos);
-    printf("Densidade Demográfica: %.2f habitantes/km²\n", populacao / area);
-    printf("PIB per Capita: %.2f bilhões de reais\n", pib / populacao);
+    printf("PIB: %.2f bilhões de reais\n", pib);
+    printf("Pontos turísticos: %d\n", pontos_turisticos);
+    printf("Densidade Demográfica: %.2f habitantes/km²\n", densidade_demografica);
+    printf("PIB per Capita: %.2f reais\n", pib_per_capita);
 
-
+    // Cadastro da Carta 2
     printf("\n=== Cadastre a Segunda Carta ===\n");
-   // Solicita e Armazena os Dados da Carta (02)
 
-    printf("Digite o Nome do Estado: \n");
-    fgets(estado, 50, stdin); // Lê o Nome do Estado
-        
-    printf("Digite o Código da Carta: \n");
-    scanf("%s", &codigo_da_carta); // Lê o Código da Carta
-    getchar();  // Remove o caractere de nova linha deixado pelo scanf
+    printf("Digite o Nome do Estado:\n");
+    fgets(estado, 50, stdin);
 
-    printf("Digite o Nome da Cidade: \n");
-    fgets(nome_da_cidade, 50, stdin); // Lê o Nome da Cidade
-   
-    printf("Digite a População: \n");
-    scanf("%d", &populacao); // Lê a População
-    getchar();  // Remove o caractere de nova linha deixado pelo scanf
+    printf("Digite o Código da Carta:\n");
+    scanf("%s", codigo_da_carta);
+    getchar();
 
-    printf("Digite a Área: \n");
-    scanf("%f", &area); // Lê a Área
-    getchar();  // Remove o caractere de nova linha deixado pelo scanf
+    printf("Digite o Nome da Cidade:\n");
+    fgets(nome_da_cidade, 50, stdin);
 
-    printf("Digite o PIB: \n");
-    scanf("%f", &pib); // Lê o PIB
-    getchar();  // Remove o caractere de nova linha deixado pelo scanf
+    printf("Digite a População:\n");
+    scanf("%d", &populacao);
+    getchar();
 
-    printf("Digite a Quantidade de Pontos Turisticos: \n");
-    scanf("%d", &pontos_turisticos); // Lê a quantidade de pontos turísticos
-    getchar();  // Remove o caractere de nova linha deixado pelo scanf
+    printf("Digite a Área:\n");
+    scanf("%f", &area);
+    getchar();
 
-    // Calculo para o PIB e Densidade Populacional
-    calcular_dados();
+    printf("Digite o PIB:\n");
+    scanf("%f", &pib);
+    getchar();
 
-    //Exibe os Dados da Carta (02)
-    
+    printf("Digite a Quantidade de Pontos Turísticos:\n");
+    scanf("%d", &pontos_turisticos);
+    getchar();
+
+    // Cálculo dos dados
+    calcular_dados(populacao, area, pib, &densidade_demografica, &pib_per_capita);
+
+    // Exibe os Dados da Carta 2
     printf("\n=== Dados da Segunda Carta ===\n");
     printf("Estado: %s", estado);
     printf("Código da Carta: %s\n", codigo_da_carta);
     printf("Cidade: %s", nome_da_cidade);
     printf("População: %d habitantes\n", populacao);
     printf("Área: %.2f km²\n", area);
-    printf("PIB: %.2f bilhões\n", pib);
-    printf("Pontos turísticos: %d \n", pontos_turisticos);
-    printf("Densidade Demográfica: %.2f habitantes/km²\n", populacao / area);
-    printf("PIB per Capita: %.2f bilhões de reais\n", pib / populacao);
+    printf("PIB: %.2f bilhões de reais\n", pib);
+    printf("Pontos turísticos: %d\n", pontos_turisticos);
+    printf("Densidade Demográfica: %.2f habitantes/km²\n", densidade_demografica);
+    printf("PIB per Capita: %.2f reais\n", pib_per_capita);
 
-    return 0; // Finaliza o Programa
+    return 0;
 }
